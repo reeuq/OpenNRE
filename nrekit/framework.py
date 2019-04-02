8150fae35e5fe77a577beca6a56566f10d75a8bd
wyd <1250608467@qq.com>
2019-04-02 10:03:34 +0800

    添加胶囊网络结构 -- 恢复framework

--- nrekit/framework.py
+++ nrekit/framework.py
@@ -23,23 +23,16 @@ def average_gradients(tower_grads):
         # Note that each grad_and_vars looks like the following:
         #     ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
         grads = []
-        flag = False
         for g, _ in grad_and_vars:
             # Add 0 dimension to the gradients to represent the tower.
-            if g is None:
-                flag = True
-                break
             expanded_g = tf.expand_dims(g, 0)
 
             # Append on a 'tower' dimension which we will average over below.
             grads.append(expanded_g)
 
         # Average over the 'tower' dimension.
-        if flag:
-            grad = None
-        else:
-            grad = tf.concat(axis=0, values=grads)
-            grad = tf.reduce_mean(grad, 0)
+        grad = tf.concat(axis=0, values=grads)
+        grad = tf.reduce_mean(grad, 0)
 
         # Keep in mind that the Variables are redundant because they are shared
         # across towers. So .. we will just return the first tower's pointer to
